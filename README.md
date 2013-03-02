7feb7bcb9ec04fd6627212ee3660b9d524503465
David Albert <davidbalbert@gmail.com>
2013-03-02 14:31:54 -0500

    Remove secret token from repo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Train Time
 
+A real-time NYC Subway schedule web app.
+
+## Developing
+
 To get started:
 
 ```
@@ -8,3 +12,7 @@ $ rake .env
 $ rake db:create
 $ foreman start
 ```
+
+## Deployment
+
+When you deploy, make sure to set the environmental variable `SECRET_TOKEN`. You can use `rake secret` to generate one.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TrainTime::Application.config.secret_token = '3668fbaf297924b261daa9dc5a6c6ff7f82e51d1913f2ea174ee8f08a5e64a6e99bb04ced3b5d7755be17f0f007771e97583e4bb17d9bf4ac5072b98543e46d9'
+TrainTime::Application.config.secret_token = ENV["SECRET_TOKEN"] || "cafebabe" * 10
