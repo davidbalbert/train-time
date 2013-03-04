89e7708b028c22a1003d2986df435933b81b8fff
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2013-03-03 22:21:57 -0500

    Terribly silly way of "querying" data
    Will probably need to switch to proper models, but was curious to see how this approach worked.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,5 +2,8 @@ class StopsController < ApplicationController
   def show
     @from = params[:from]
     @favorited = true
+    stop_id = params[:id]
+    @stop_name = get_stop_name(stop_id)
+    @lines = get_lines(stop_id)
   end
 end
