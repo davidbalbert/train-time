501d5e4b50d8fbfd805fe06348072fa3188ccb33
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2013-03-03 22:46:11 -0500

    Consistent capitalization; use path helpers

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,6 +1,6 @@
 class LinesController < ApplicationController
   def show
     @line_num = params['id']
-    @stops = STOPS_BY_LINE[@line_num.upcase]
+    @stops = STOPS_BY_LINE[@line_num]
   end
 end
